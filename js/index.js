6689733e3b6e1cc2d6ce41051d8a7264d1e86fdc
codersjj <shanewestlife@outlook.com>
2025-09-16 14:55:31 +0800

    feat: add checkbox component

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 // Write custom JavaScript here.
 // You may ignore this file and delete if if JavaScript is not required for your challenge.
+const indeterminateCheckbox = document.getElementById('indeterminate')
+indeterminateCheckbox.indeterminate = true
